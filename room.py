9a48b6aa8b46c01bdec270d66061ad6cbff8347d
Shreesh Shukla <shreesh8210@gmail.com>
2022-12-31 15:38:31 -0800

    first commit

---
+++ room.py
@@ -0,0 +1,12 @@
+X=int(input())
+for i in range(1,X):
+    a=int(input())
+    b=int(input())
+    print(a)
+    print(b)
+    if a>b:
+        print("FIRST")
+    elif a<b:
+        print("SECOND")
+    else:
+        print("ANY")
